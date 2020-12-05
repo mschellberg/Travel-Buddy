a3196234cd72a01789658b0a8db9bfcdbe872f5b
steftinsley <69828366+steftinsley@users.noreply.github.com>
2020-12-05 15:40:34 -0600

    added hamburger menu functionality

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -15,6 +15,32 @@ var currentCityTempTwo = document.getElementById("current-city-temp-two");
 var currentCityWindSpeedTwo = document.getElementById("current-city-wind-two");
 var currentCityHumidityTwo = document.getElementById("current-city-humidity-two");
 
+document.addEventListener('DOMContentLoaded', () => {
+
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+  // Check if there are any navbar burgers
+  if ($navbarBurgers.length > 0) {
+
+    // Add a click event on each of them
+    $navbarBurgers.forEach( el => {
+      el.addEventListener('click', () => {
+
+        // Get the target from the "data-target" attribute
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
+
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+
+      });
+    });
+  }
+
+});
+
 
 //to be executed upon a form submission browser event 
 var formSubmitHandler = function(event) {
