a3ea708f936678b827831777c004e9d593440f36
Melanie <mschellberg27@icloud.com>
2020-12-08 16:36:39 -0500

    location b get planning button works

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,40 @@
 // Setting up the map - Map A
 function initMap() {
     var philadelphia = new google.maps.LatLng(39.9526, -75.1652);
-        infowindow = new google.maps.InfoWindow();
-    map = new google.maps.Map(
+        //infowindow = new google.maps.InfoWindow();
+        map = new google.maps.Map(
         document.getElementById('map'), {center: philadelphia, zoom:13});
 
-        const marker = new google.maps.Marker({
+        var marker = new google.maps.Marker({
             position: philadelphia,
             map: map,
             draggable: true
-        });
+    });
+
+// Map A Search Box
+var input = document.getElementById("autocomplete");
+var searchBox = new google.maps.places.SeachBox(input);
+
 // Map A Search Box
     var input = document.getElementById("autocomplete");
     var searchBox = new google.maps.places.SearchBox(input);
 
     google.maps.event.addListener(searchBox, 'places_changed', function(){
         var places = searchBox.getPlaces();
-       
-        var srcLocation = $(input).val();
 // Location search appears in the title element
+        var srcLocation = $(input).val();
         $("#locationTitle").text(srcLocation);
         //saveLocation(location);
-        
+
         var bounds = new google.maps.LatLngBounds ();
         for(i=0; place=places[i];i++){
             bounds.extend(place.geometry.location);
             marker.setPosition(place.geometry.location);
-        }
+    }
         map.fitBounds(bounds);
         map.setZoom(15);
     });
+
 
 // Map B
     var philadelphia2 = new google.maps.LatLng(39.9526, -75.1652);
@@ -37,7 +42,7 @@ function initMap() {
     new google.maps.Map(
         document.getElementById('map2'), {center: philadelphia2, zoom:13});
 
-        const marker2 = new google.maps.Marker({
+        var marker2 = new google.maps.Marker({
             position: philadelphia2,
             map: map2,
             draggable: true
@@ -45,6 +50,7 @@ function initMap() {
 // Map B Search Box
     var input2 = document.getElementById("autocomplete2");
     var searchBox2 = new google.maps.places.SearchBox(input2);
+    
     google.maps.event.addListener(searchBox2, 'places_changed', function(){
         var places2 = searchBox2.getPlaces();
         destinationLocation = $(input2).val();
@@ -54,7 +60,7 @@ function initMap() {
         var bounds2 = new google.maps.LatLngBounds ();
         for(i=0; place=places2[i];i++){
             bounds2.extend(place.geometry.location);
-            marker.setPosition(place.geometry.location);
+            marker2.setPosition(place.geometry.location);
         }
 
         map2.fitBounds(bounds2);
@@ -221,4 +227,4 @@ var displayCurrentWeather =function (weatherData) {
  
 //event listeners
 userFormOneEl.addEventListener("submit", formSubmitHandler);
-userFormTwoEl.addEventListener("submit", formSubmitHandlerTwo);
+//userFormTwoEl.addEventListener("submit", formSubmitHandlerTwo);
